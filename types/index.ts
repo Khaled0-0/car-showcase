9198818fcacd52f98877d484449d5efc4dfed6da
Khaledabdelnasserr <khaledmaster999@gmail.com>
2025-04-30 19:58:31 +0300

    first push

---
+++ types/index.ts
@@ -0,0 +1,15 @@
+export interface CustomButtonProps {
+   title: string;
+   containerStyles?: string;
+   handleClick?: MouseEventHandler<HTMLButtonElement>;
+   btnType?: "button" | "submit";
+   textStyles?: string;
+   rightIcon?: string;
+   isDisabled?: boolean;
+}
+
+
+export interface SearchManufacturerProps {
+   manufacturer: string;
+   setManufacturer: (manufacturer: string) => void;
+}
