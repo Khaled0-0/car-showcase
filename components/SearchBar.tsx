77af7124d96dd89c3a7d92175c9ad5754da2446f
Khaledabdelnasserr <khaledmaster999@gmail.com>
2025-04-30 20:08:19 +0300

    Edit SearchBar.tsc component

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,7 +5,7 @@ import SearchManufacturer from './SearchManufacturer'
 import { useState } from 'react'
 
 const SearchBar = () => {
-   const { manufacturer, setManufacturer } = useState('')
+   const [manufacturer, setManufacturer] = useState('')
    const handleSearch = () => { }
 
 
