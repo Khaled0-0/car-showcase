9f996cb332c60ed7bf9a522d1e5316c9798d7509
Khaledabdelnasserr <khaledmaster999@gmail.com>
2025-05-01 19:18:03 +0300

    Add Category function and APIs

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -12,9 +12,14 @@ const CustomButton = ({ title, containerStyles, handleClick, btnType, textStyles
          type={btnType || "button"}
          onClick={handleClick}
       >
-         <span className={`flex-1`}>
+         <span className={`flex-1 ${textStyles}`}>
             {title}
          </span>
+         {rightIcon && (
+            <div className='relative w-6 h-6'>
+               <Image src={rightIcon} alt='right icon' fill className='object-contain' />
+            </div>
+         )}
       </button>
    )
 }
--- components/index.ts
+++ components/index.ts
@@ -5,6 +5,8 @@ import Footer from "./Footer";
 import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomFilter";
 import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
 
 
 
@@ -17,4 +19,6 @@ export {
    SearchBar,
    CustomFilter,
    SearchManufacturer,
+   CarCard,
+   CarDetails,
 }
